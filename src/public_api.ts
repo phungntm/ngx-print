11781162d1e5b49913514b732a07c3f04f2a4345
Corey Franco <42871786+Core121@users.noreply.github.com>
2023-11-22 15:07:36 -0500

    Export printservice and interface

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,6 +1,7 @@
 /*
  * Public API Surface of ngx-print
  */
-
+export * from './lib/ngx-print.service';
 export * from './lib/ngx-print.directive';
 export * from './lib/ngx-print.module';
+export * from './lib/iprint-options';
